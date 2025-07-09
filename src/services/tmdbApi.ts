b253964d7820fb8f549075429e38b3f0a9ad9383
gaurav091100 <gaurav091100@gmail.com>
2025-07-10 01:01:44 +0530

    fixed production api base url issue

--- src/services/tmdbApi.ts
+++ src/services/tmdbApi.ts
@@ -3,7 +3,7 @@
 import axios from 'axios';
 
 const API_KEY = import.meta.env.VITE_API_KEY;
-const BASE_URL = '/api';
+const BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 const api = axios.create({
   baseURL: BASE_URL,
--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,10 @@
 import path from "path"
 import tailwindcss from "@tailwindcss/vite"
-import { defineConfig,loadEnv  } from 'vite'
+import { defineConfig  } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, process.cwd());
-
+export default defineConfig(() => {
   return {
   plugins: [react(), tailwindcss()],
   resolve: {
@@ -14,14 +12,5 @@ export default defineConfig(({ mode }) => {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  server: {
-    proxy: {
-      '/api': {
-        target: env.VITE_API_BASE_URL,
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
-     },
-      },
-    },
   };
 });
